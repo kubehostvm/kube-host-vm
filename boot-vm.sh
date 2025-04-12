d4b16696d611c4fd52e1768cfe36d7a501ae1312
zbb88888 <jmdxjsjgcxy@gmail.com>
2025-04-12 09:45:35 +0800

    test
    Signed-off-by: zbb88888 <jmdxjsjgcxy@gmail.com>

--- boot-vm.sh
+++ boot-vm.sh
@@ -2,21 +2,16 @@
 set -eux
 # pod env
 # init var
-if [ -z "${POD_UID}" ]; then
-    echo "POD_UID is not set. Exiting."
-    exit 1
-fi
-last12=${POD_UID: -12}
-dev="tap${last12}"
+
 if [ -z "${POD_NAME}" ]; then
     echo "POD_NAME is not set. Exiting."
     exit 1
 fi
-# todo:// LSPs for multi nic
-if [ -z "${LSP}" ]; then
-    echo "LSP is not set. Exiting."
+if [ -z "${POD_NAMESPACE}" ]; then
+    echo "POD_NAMESPACE is not set. Exiting."
     exit 1
 fi
+vmname="${POD_NAMESPACE}-${POD_NAME}"
 if [ -z "${CPU}" ]; then
     echo "CPU is not set. Exiting."
     exit 1
@@ -27,15 +22,17 @@ if [ -z "${MAC_ADDR}" ]; then
     exit 1
 fi
 
+smp=$((2 * CPU))
+
 qemu-system-x86_64 \
-  -name "${POD_NAME}" \
+  -name "${vmname}" \
   -m 2G \
-  -smp "${CPU}" \
+  -smp "${smp},cores=${CPU}" \
   -boot menu=on \
-  -drive file=/etc/kube-host-vm/"${POD_NAME}".qcow2,format=qcow2,if=virtio \
+  -drive file=/etc/kube-host-vm/"${POD_NAME}".qcow2,format=qcow2,if=virtio,cache=unsafe \
   -drive file=/etc/kube-host-vm/"${POD_NAME}"-cloud-cfg.img,format=raw,if=virtio,media=cdrom \
   -nographic \
   -vnc :0 \
-  -netdev tap,id=net0,ifname="${dev}",script=/etc/kube-host-vm/tap-into-ovs.sh,downscript=/etc/kube-host-vm/untap-into-ovs.sh,vhost=on \
-  -device virtio-net-pci,netdev=net0,mac="${MAC_ADDR}" \
+  -netdev tap,id=net0,script=/etc/kube-host-vm/tap-into-ovs.sh,downscript=/etc/kube-host-vm/untap-into-ovs.sh,vhost=on \
+  -device virtio-net-pci,netdev=net0,id=nic0,mac="${MAC_ADDR}" \
   -device virtio-rng-pci -enable-kvm -cpu host,+x2apic
