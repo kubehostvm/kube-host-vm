5d5d52f312a46175babfd613a4fc931f0e663a1e
zbb88888 <jmdxjsjgcxy@gmail.com>
2025-04-13 15:46:46 +0800

    wait 5s to delete, standard img dir
    Signed-off-by: zbb88888 <jmdxjsjgcxy@gmail.com>

--- boot-vm.sh
+++ boot-vm.sh
@@ -3,8 +3,8 @@ set -eux
 # pod env
 # init var
 
-BASE_IMG="/etc/kube-host-vm/noble-server-cloudimg-amd64.img"
-BASE_IMG_CLOUD_CFG="/etc/kube-host-vm/cloud-cfg.raw"
+BASE_IMG="/etc/kube-host-vm/img/noble-server-cloudimg-amd64.img"
+BASE_IMG_CLOUD_CFG="/etc/kube-host-vm/img/cloud-cfg.raw"
 
 if [ -z "${POD_NAME}" ]; then
     echo "POD_NAME is not set. Exiting."
@@ -37,7 +37,7 @@ if [ -z "${CPU}" ]; then
 fi
 
 smp=$((2 * CPU))
-qcow2_file="/etc/kube-host-vm/${name}.qcow2"
+qcow2_file="/etc/kube-host-vm/img/${name}.qcow2"
 if [ ! -f "${qcow2_file}" ]; then
     echo "qcow2 file ${qcow2_file} does not exist, creating."
     if [ ! -f "${BASE_IMG}" ]; then
@@ -47,7 +47,7 @@ if [ ! -f "${qcow2_file}" ]; then
     cp "${BASE_IMG}" "${qcow2_file}"
 
 fi
-cloud_cfg_file="/etc/kube-host-vm/${name}-cloud-cfg.raw"
+cloud_cfg_file="/etc/kube-host-vm/img/${name}-cloud-cfg.raw"
 # cloud config file is used to inject into the vm cloud-init
 if [ ! -f "${cloud_cfg_file}" ]; then
     echo "cloud cfg file ${cloud_cfg_file} does not exist, creating."
@@ -64,7 +64,10 @@ if [ "$CPU" -lt 2 ]; then
 fi
 
 # get current vnc index
+set +e
 vnc_index=$(ss -tunlp | grep -c "0.0.0.0:59")
+set -e
+
 using_vnc_port=$((5900 + vnc_index))
 echo "vm ${name} using vnc port ${using_vnc_port}"
 
